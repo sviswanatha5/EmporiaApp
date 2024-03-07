9f5538c53ab944cb4211fa0ce7ccc76056f5ae47
Varun Patel <varunpatel@Varuns-MacBook-Pro-2.local>
2024-03-07 13:09:35 -0500

    new home screen + register pref

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/srikarviswanatha/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/srikarviswanatha/GTCS/CS4261/EmporiaApp-1"
+export "FLUTTER_ROOT=/Users/varunpatel/Desktop/tech/Frameworks/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/varunpatel/Desktop/CS 4261/project/EmporiaApp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/varunpatel/Desktop/CS 4261/project/EmporiaApp/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNDBlOTc2YmVkZmY1N2U2OWUxYjNkODlhN2MyYTNjNjE3YTAzZGFkLw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/varunpatel/Desktop/CS 4261/project/EmporiaApp/.dart_tool/package_config.json"
