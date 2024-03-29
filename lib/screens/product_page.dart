4089674bc04795a06fe1eac8d1b889aa39bb5979
sviswanatha6 <sviswanatha6@gatech.edu>
2024-03-28 21:36:18 -0400

    added searchbar into UI

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -1,3 +1,4 @@
+import "package:cached_network_image/cached_network_image.dart";
 import "package:cloud_firestore/cloud_firestore.dart";
 import "package:flutter/material.dart";
 import "package:practice_project/components/background.dart";
@@ -24,21 +25,19 @@ class Product {
     false
   ];
 
-  Product({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.price,
-    required this.images,
-    required this.vendor,
-    required this.isLiked,
-    required this.productGenre
-  });
+  Product(
+      {required this.id,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.images,
+      required this.vendor,
+      required this.isLiked,
+      required this.productGenre});
 }
 
 final CollectionReference products =
-      FirebaseFirestore.instance.collection('products');
-
+    FirebaseFirestore.instance.collection('products');
 
 class ProductPage extends StatefulWidget {
   ProductPage({Key? key}) : super(key: key) {
@@ -49,10 +48,12 @@ class ProductPage extends StatefulWidget {
 
   @override
   State<ProductPage> createState() => _ProductPageState();
+
+  @override
+  
 }
 
 class _ProductPageState extends State<ProductPage> {
-  
   //retrieve all products from Firestore
   Future<List<Product>> getProducts() async {
     QuerySnapshot querySnapshot = await products.get();
@@ -67,7 +68,7 @@ class _ProductPageState extends State<ProductPage> {
         id: data['id'],
         price: data['price'],
         name: data['name'],
-        images: data['images'],
+        images: (data['images'] as List<dynamic>).map((image) => image.toString()).toList(),
         isLiked: data['isLiked'],
         vendor: data['vendor'],
         productGenre: List<bool>.from(data['productGenre']),
@@ -77,94 +78,119 @@ class _ProductPageState extends State<ProductPage> {
     return productList;
   }
 
-  Future<void> loadProducts() async {
-    List<Product> loadedProducts = await getProducts();
-    loadedProductList = loadedProducts;
+  @override
+  void initState() {
+    super.initState();
+    loadProducts();
   }
 
   List<Product> loadedProductList = [];
-    final TextEditingController _searchController = TextEditingController();
+  List<Product> filteredProductList = [];
 
+  Future<void> loadProducts() async {
+    List<Product> loadedProducts = await getProducts();
+    loadedProductList = loadedProducts;
+    filteredProductList = List.from(loadedProducts);
+  }
+
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-        decoration: gradientDecoration(),
-        child: StreamBuilder<QuerySnapshot>(
-            stream: widget.stream,
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.hasError) {
-                return const Center(child: Text('Some error occured'));
-              }
+            decoration: gradientDecoration(),
+            child: StreamBuilder<QuerySnapshot>(
+                stream: widget.stream,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasError) {
+                    return const Center(child: Text('Some error occured'));
+                  }
 
-              if (snapshot.hasData) {
-                QuerySnapshot querySnapshot = snapshot.data;
-                List<QueryDocumentSnapshot> documents = querySnapshot.docs;
+                  if (snapshot.hasData) {
+                    QuerySnapshot querySnapshot = snapshot.data;
+                    List<QueryDocumentSnapshot> documents = querySnapshot.docs;
 
-                List<Product> items = documents.map((data) {
-                  return Product(
-                    id: data['id'],
-                    name: data['name'],
-                    description: data['description'],
-                    price: data['price']
-                        .toDouble(), // Assuming 'price' is stored as a double
-                    images: List<String>.from(data['images']),
-                    isLiked: data['isLiked'],
-                    vendor: data['vendor'],
-                    productGenre: List<bool>.from(data['productGenre'])
-                  );
-                }).toList();
+                    List<Product> items = documents.map((data) {
+                      return Product(
+                          id: data['id'],
+                          name: data['name'],
+                          description: data['description'],
+                          price: data['price']
+                              .toDouble(), // Assuming 'price' is stored as a double
+                          images: (data['images'] as List<dynamic>).map((image) => image.toString()).toList(),
+                          isLiked: data['isLiked'],
+                          vendor: data['vendor'],
+                          productGenre: List<bool>.from(data['productGenre']));
+                    }).toList();
 
-                return Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: TextField(
-                        controller: _searchController,
-                        decoration: const InputDecoration(
-                          labelText: 'Search',
-                          suffixIcon: Icon(Icons.search),
+                    void filterProducts(String query) {
+                      setState(() {
+                        if (query.isNotEmpty) {
+                          // Otherwise, filter products based on the search query
+                          filteredProductList = items.where((product) {
+                            // Check if the product name contains the search query
+                            return product.name
+                                .toLowerCase()
+                                .contains(query.toLowerCase());
+                          }).toList();
+
+                          print(query);
+                        } else {
+                          filteredProductList = items;
+                        }
+                      });
+                    }
+
+                    List<Product> filteredItems = filteredProductList;
+
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: TextField(
+                            controller: _searchController,
+                            onSubmitted: (String value) {
+                              filterProducts(value);
+                            },
+                            decoration: const InputDecoration(
+                              labelText: 'Search',
+                              suffixIcon: Icon(Icons.search),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    Expanded(
-                      child:  ListView.builder(
-                        itemCount: items.length,
-                        itemBuilder: (context, index) {
-                          return Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SquareTileProduct(
-                                  product: items[index],
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            ProductDetailScreen(items[index]),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ]);
-                        },
-                     ),
-                    ),
-                  ],
-                );
-              }
-              else{
-                return const Center(child: CircularProgressIndicator());
-                
-              }
-
-              
-            })));
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: filteredItems.length,
+                            itemBuilder: (context, index) {
+                              return Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SquareTileProduct(
+                                      product: filteredItems[index],
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                ProductDetailScreen(
+                                                    filteredItems[index]),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ]);
+                            },
+                          ),
+                        ),
+                      ],
+                    );
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                })));
   }
 }
-
-
 
 class ProductDetailScreen extends StatelessWidget {
   final Product product;
@@ -174,41 +200,45 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.name),
-      ),
-      body: Container(
-        decoration: gradientDecoration(),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(product.images.first),
-            // Display more pictures here using product.images
-            // Use Image.network or Image.asset depending on where your images are located
-            Text(
-              'Price: \$${naturalPrices(product.price)}',
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Description: ${product.description}',
-              style: const TextStyle(fontSize: 16),
-            ),
-
-            const SizedBox(height: 10),
-            Text(
-              'Contact: ${product.vendor}',
-              style: const TextStyle(fontSize: 16),
-            ),
-            // Add more details as needed
-
-            const SizedBox(height: 100),
-
-            MyButton(onTap: () => {}, text: "Connect"),
-          ],
+        appBar: AppBar(
+          title: Text(product.name),
         ),
-      ),
-    ));
+        body: Container(
+          decoration: gradientDecoration(),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CachedNetworkImage(
+                imageUrl: product.images.first,
+                fit: BoxFit.cover,
+              ),
+                // Display more pictures here using product.images
+                // Use Image.network or Image.asset depending on where your images are located
+                Text(
+                  'Price: \$${naturalPrices(product.price)}',
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Description: ${product.description}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+
+                const SizedBox(height: 10),
+                Text(
+                  'Contact: ${product.vendor}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+                // Add more details as needed
+
+                const SizedBox(height: 100),
+
+                MyButton(onTap: () => {}, text: "Connect"),
+              ],
+            ),
+          ),
+        ));
   }
 }
