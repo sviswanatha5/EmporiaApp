ffeb73e5f17b93de983cf00c589ee080550f756b
Varun Patel <varunpatel@Varuns-MacBook-Pro-2.local>
2024-03-29 13:06:51 -0400

    app bar and nav bar gradient design

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -12,3 +12,19 @@ BoxDecoration gradientDecoration() {
     ),
   );
 }
+
+BoxDecoration gradientDecoration2() {
+  return BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [
+        Color(0xFF9A69AB).withOpacity(0.8), // Starting with the contrasting color but with less opacity for subtlety
+        Color(0xFFC4A5E8).withOpacity(0.8), // A lighter shade of purple but with more presence
+        Color(0xFFFF6F61).withOpacity(0.8), // Ending with the dark purple
+        
+
+      ],
+    ),
+  );
+}
--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,13 +1,11 @@
-import "package:firebase_auth/firebase_auth.dart";
-import "package:flutter/material.dart";
-import "package:practice_project/dashboard_widgets/add_product.dart";
-import "package:practice_project/dashboard_widgets/favorites.dart";
-import "package:practice_project/dashboard_widgets/profile.dart";
-import "package:practice_project/dashboard_widgets/searchbar.dart";
-import "package:practice_project/screens/for_you_page.dart";
-import "package:practice_project/screens/messages_screen.dart";
-import "package:practice_project/screens/product_page.dart";
-import "package:practice_project/components/background.dart";
+import 'package:flutter/material.dart';
+import 'package:practice_project/components/background.dart';
+import 'package:practice_project/dashboard_widgets/add_product.dart';
+import 'package:practice_project/dashboard_widgets/favorites.dart';
+import 'package:practice_project/dashboard_widgets/profile.dart';
+import 'package:practice_project/screens/for_you_page.dart';
+import 'package:practice_project/screens/messages_screen.dart';
+import 'package:practice_project/screens/product_page.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -18,65 +16,74 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   int _selectedIndex = 0;
-  
-  
 
-  
-
-
-
-
-  List<Widget> get widgetOptions =>
-      [ForYouPage(),  ProductPage(), FavoriteProducts(), MessagesScreen(), AddProduct(),  ProfileWidget()];
+  List<Widget> get widgetOptions => [
+        ForYouPage(),
+        ProductPage(),
+        FavoriteProducts(),
+        MessagesScreen(),
+        AddProduct(),
+        ProfileWidget()
+      ];
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Emporia'),
-    ),
-    body: Container(
-      decoration: gradientDecoration(),
-      child: Center(
-        child: widgetOptions.elementAt(_selectedIndex),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Emporia'),
+        flexibleSpace: Container(
+          decoration: gradientDecoration2(),
+        ),
+        elevation: 0,
       ),
-    ),
-    bottomNavigationBar: BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'For you',
+      body: Container(
+        decoration: gradientDecoration(),
+        child: Center(
+          child: widgetOptions.elementAt(_selectedIndex),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Explore',
+      ),
+      bottomNavigationBar: Container(
+        decoration: gradientDecoration2(),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'For you',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Explore',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble_outlined),
+              label: 'Connect',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_outlined),
+              label: 'Sell',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_rounded),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.white70,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.favorite),
-          label: 'Favorites',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.chat_bubble_outlined),
-          label: 'Connect',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.add_box_outlined),
-          label: 'Sell',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.account_circle_rounded),
-          label: 'Profile',
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Colors.deepPurple,
-      onTap: (index) {
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
-    ),
-  );
-}
+      ),
+    );
+  }
 }
